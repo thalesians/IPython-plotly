c2a4ea0dca12cb8792dcfa011da68e0ccbe31654
etpinard <etienne.t.pinard@gmail.com>
2015-03-09 16:23:18 -0400

    add support for ipython 3.0 (nbformat 4)

--- _makescripts/trim.py
+++ _makescripts/trim.py
@@ -5,6 +5,13 @@ import common
 # -------------------------------------------------------------------------------
 
 
+def get_slice(array, cells):
+    if cells[1] == 'end':
+        return array[cells[0]:]
+    else:
+        return array[cells[0]:cells[1]]
+
+
 def main():
 
     path_handler = common.PathHandler('trim')
@@ -15,11 +22,16 @@ def main():
         nb_json = ds.load_json(file_ipynb)
         cells = path_handler.load_config(arg)['cells']
 
-        nb_json_cells = nb_json['worksheets'][0]['cells']
-        nb_json['worksheets'][0]['cells'] = nb_json_cells[cells[0]:cells[1]]
+        if nb_json['nbformat'] == 4:
+            nb_json_cells = nb_json['cells']
+            nb_json['cells'] = get_slice(nb_json_cells, cells)
+        else:
+            nb_json_cells = nb_json['worksheets'][0]['cells']
+            nb_json['worksheets'][0]['cells'] = get_slice(nb_json_cells, cells)
 
         file_tmp_ipynb = file_ipynb.replace('.ipynb', '.tmp.ipynb')
         ds.dump_json(nb_json, file_tmp_ipynb, indent=1)
+
 
 if __name__ == "__main__":
     main()
