327a830568ddca240de75c7b097afb723a50d8e8
etpinard <etienne.t.pinard@gmail.com>
2015-03-12 14:52:19 -0400

    make run-publish

--- _published/sitemaps.py
+++ _published/sitemaps.py
@@ -6,6 +6,19 @@ from django.conf import settings
 def items():
     items = [
         dict(
+            location='/ipython-notebooks/graph-gmail-inbox-data',
+            lmfile=os.path.join(
+                settings.TOP_DIR,
+                'shelly',
+                'templates',
+                'api_docs',
+                'includes',
+                'ipython_notebooks',
+                'gmail',
+                'body.html'),
+            priority=0.5
+        ),
+        dict(
             location='/ipython-notebooks/markowitz-portfolio-optimization',
             lmfile=os.path.join(
                 settings.TOP_DIR,
